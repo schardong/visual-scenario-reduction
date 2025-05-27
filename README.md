56dd5321b61de4ae63586c113139852664a6c7e3
Guilherme Schardong <schardong.gg@gmail.com>
2025-05-26 23:46:31 -0300

    Linting and README update.

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ This is the official implementation of "Visual interactive support for selecting
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-python mainwindow.py
+python mainwindow.py --input-path data_50/
 ```
 
 ### Prerequisites
@@ -25,11 +25,45 @@ python mainwindow.py
 3. [Qt 5](https://doc.qt.io/qt-5/)
 
 ### Code organization
+Our codebase resides entirely on the repository root. It is divided in three main categories: Plot, Data and Miscellaneous classes/files. We list them below.
+
+#### Plot classes
+* `brushableplot.py` - base class for all plots that support connected-views, a.k.a. "Brushing and Linking"
+* `distplot.py` -  distance chart class definition. See an example at the bottom right of Fig. 1
+* `fanchart.py` - fan chart class definition. See an example at the top left of Fig. 1
+* `rankplot.py` - bump chart class definition. See an example at the bottom left of Fig. 1
+* `tlplot.py` - time-lapsed LAMP chart class definition. See an example at the top right of Fig. 1
+
+#### Data classes
+These are the only domain specific classes contained here. They are mostly related to oil-reservoir data types. Replace them for your use case. You will also need to replace the loading function in `mainwindow.py`.
+
+* `field.py` - information on a field scenario with all wells' types, names, properties, data and meta-information on the scenario itself (an ID, really)
+* `fieldensemble.py` - a set of fields with well types and observed data. Allows querying group-data, historic data (not implemented), well type (not implemented)
+* `timeseries.py` - class that contains a thin-wrapper for multidimensional time-series data. Should be replaced by `pandas.DataFrames` really
+
+### Miscellaneous
+* `mp.py` - dimesionality reduction methods implementation: MDS, LAMP, TL-LAMP
+* `mainwindow.py` - main window of the prototype, also it's entry point. Contains a plot widget, menu, and side-bar for interaction
+* `plotwidget.py` - plot widget containing all plots. Handles their communication
+* `zoompanhandler.py` - zoom+pan interaction handler used in some of our plots
+* `zoompantest.py` - test file for the zoom+pan handler
 
 ### Setup and sample run
+Assuming you have Qt5 installed, this setup is straightforward. Simply create a virtual environment to isolate your Python installation, install the requirements and run the prototype. These steps are shown below, assuming an UNIX-like system with Python `v3.13.1`, [pyenv](https://github.com/pyenv/pyenv), and [pyenv-virtualenv](https://github.com/pyenv/pyenv-virtualenv) installed.
+
+```{sh}
+cd $PATH_TO_REPOSITORY_ROOT
+pyenv virtualenv 3.13.1 tsvis
+pyenv activate tsvis
+pip install -r requirements.txt
+python mainwindow.py --input-path data_50/
+```
+
+If everything works well, you should see the following window:
+![Fig. 1: Main window of our prototype.](img/mainwindow.png)
 
 ## Contributing
-Any contribution is welcome. If you spot an error or have questions, open issues and we will answer as soon as possible.
+Any contribution is welcome. If you spot an error or have questions, open issues, or send an e-mail and we will answer as soon as possible.
 
 ## Citation
 If you find our work useful in your research, consider citing it in your tech report or paper.
--- fieldensemble.py
+++ fieldensemble.py
@@ -2,16 +2,19 @@ import os
 import numpy as np
 from field import Field
 
+
 class FieldEnsemble:
-    '''
+    """
     TODO(gschardong):
     1) Read well type data;
     2) Read observed data;
     3) Return data by well type (done in Field);
     4) Return only historic or forecast data;
-    '''
+    """
 
-    def __init__(self, well_data_path='', prop_list=[], well_type_path='', obs_data_path=''):
+    def __init__(
+        self, well_data_path="", prop_list=[], well_type_path="", obs_data_path=""
+    ):
         self._fields = {}
         self._well_types = {}
         self._obs_data = np.zeros(shape=(1, 1))
@@ -24,38 +27,38 @@ class FieldEnsemble:
             self.load_observed_data(path=obs_data_path)
 
     def __len__(self):
-        '''
+        """
         Returns the number of fields stored in this ensemble.
-        '''
+        """
         return len(self._fields)
 
     @property
     def field_names(self):
-        '''
+        """
         Returns the names of the fields of this ensemble.
-        '''
+        """
         return list(self._fields.keys())
 
     @property
     def well_names(self):
-        '''
+        """
         Returns the well names of the fields in this ensemble. It is expected
         that all fields have the same wells.
-        '''
+        """
         return list(self._well_types.keys())
-    
+
     @property
     def well_types(self):
-        '''
+        """
         Returns the types of wells for the fields in this ensemble.
 
         Returns:
         A dictionary with the wells' names as keys and their types as values.
-        '''
+        """
         return self._well_types
 
-    def get_well_data(self, well_name='', well_prop=''):
-        '''
+    def get_well_data(self, well_name="", well_prop=""):
+        """
         Returns the data of a property, or list of properties of a well from
         all fields.
 
@@ -66,12 +69,11 @@ class FieldEnsemble:
         Returns -- A numpy.ndarray for each property selected. If more than one
         property is selected, then a dictionary with the properties as keys is
         returned.
-        '''
+        """
         raise NotImplementedError
 
-    def get_group_data(self, well_type='', well_prop=''):
-        '''
-        '''
+    def get_group_data(self, well_type="", well_prop=""):
+        """ """
         if isinstance(well_prop, str):
             well_prop = [well_prop]
 
@@ -93,7 +95,7 @@ class FieldEnsemble:
         return group_data
 
     def get_field(self, name):
-        '''
+        """
         Returns a field with the given name. If no field is found, then None
         will be returned instead. If a list of names is given, then a dictionary
         will be returned.
@@ -105,7 +107,7 @@ class FieldEnsemble:
         Returns:
         A single field (if name is a string) or a dictionary with the
         corresponding fields (if name is a list).
-        '''
+        """
         if isinstance(name, str):
             return self._fields[name]
         elif isinstance(name, list):
@@ -115,29 +117,29 @@ class FieldEnsemble:
             return fields
 
     def add_field(self, field):
-        '''
+        """
         Adds a new field to the ensemble. Raises ValueError if the Field is
         invalid (None or empty).
 
         Arguments:
         field -- The field to be added.
-        '''
+        """
         self._fields[field.name] = field
 
     def rem_field(self, field_name):
-        '''
+        """
         Removes a field from the ensemble. Raises ValueError if the field is
         not in the ensemble.
 
         Arguments:
         field_name -- The name of the field to remove.
-        '''
+        """
         if not field_name in self._fields:
-            raise ValueError('Field not in ensemble.')
+            raise ValueError("Field not in ensemble.")
         self._fields[field_name] = None
 
     def load_fields(self, base_path, prop_list=[]):
-        '''
+        """
         Reads all CSV files in the directory indicated as Fields. Raises
         ValueError if the path is invalid.
 
@@ -145,9 +147,9 @@ class FieldEnsemble:
         base_path -- Root path to the CSV files.
         prop_list -- The list of properties to be read. Must be folder names
         under 'base_path'.
-        '''
+        """
         if not os.path.exists(base_path):
-            raise ValueError('Invalid path.')
+            raise ValueError("Invalid path.")
 
         if isinstance(prop_list, str):
             prop_list = [prop_list]
@@ -157,13 +159,13 @@ class FieldEnsemble:
         fieldnames = os.listdir(os.path.join(base_path, prop_list[0]))
 
         for field in fieldnames:
-            field = field.split('.')[0]
-            f = Field();
+            field = field.split(".")[0]
+            f = Field()
             f.load_field(base_path, prop_list, field)
             self.add_field(f)
 
     def load_well_types(self, path):
-        '''
+        """
         Loads the well types for the fields of this ensemble. If there are
         fields in the ensemble, then their corresponding set_well_types method
         is called.
@@ -172,11 +174,13 @@ class FieldEnsemble:
         path -- Path to the well type file. This file must be in CSV format
         with the well names as a header and the well types as the only data.
         The file must be named 'welltypes.csv'.
-        '''
-        filename = 'welltype.csv'
-        with open(os.path.join(path, filename), 'r') as fin:
+        """
+        filename = "welltype.csv"
+        with open(os.path.join(path, filename), "r") as fin:
             contents = fin.readlines()
-            names, types = contents[0].strip().split(','), contents[1].strip().split(',')
+            names, types = contents[0].strip().split(","), contents[1].strip().split(
+                ","
+            )
             self._well_types = dict(zip(names, types))
 
             if len(self):
@@ -184,28 +188,30 @@ class FieldEnsemble:
                     self._fields[k].set_well_types(self.well_types)
 
     def load_observed_data(self, path):
-        '''
+        """
         Loads the observed data for the fields.
-        '''
+        """
         if not path or not path:
-            raise ValueError('Invalid path, no observed data loaded.')
+            raise ValueError("Invalid path, no observed data loaded.")
 
-        filenames = os.listdisr(path)
+        filenames = os.listdir(path)
         for f in filenames:
             pass
 
-def test_FieldEnsemble():
-    PROPERTY = ['NP', 'WP']
-    BASE_DATA_PATH = os.path.join('..', 'data', 'ajustado')
 
-    fe = FieldEnsemble(well_data_path=BASE_DATA_PATH, well_type_path='../data')
+def test_FieldEnsemble():
+    PROPERTY = ["NP", "WP"]
+    BASE_DATA_PATH = os.path.join("..", "data", "ajustado")
+
+    fe = FieldEnsemble(well_data_path=BASE_DATA_PATH, well_type_path="../data")
     print(fe.well_types)
 
-    group_data_dict = fe.get_group_data(well_prop=PROPERTY, well_type='P')
+    group_data_dict = fe.get_group_data(well_prop=PROPERTY, well_type="P")
 
     for gdata in group_data_dict:
         print(len(gdata))
         print(gdata.shape)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_FieldEnsemble()
